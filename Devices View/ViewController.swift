e261b4762d78bdbebfde8d62c48b263b84bb02c9
Steven Hertz <hertzsteven@icloud.com>
2020-03-04 18:19:52 -0500

    Initial Commit

---
+++ Devices View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Devices View
+//
+//  Created by Steven Hertz on 3/4/20.
+//  Copyright © 2020 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
